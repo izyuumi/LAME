bb402f854a2823feab5d2449289619df00f70318
Yumi Izumi <me@yumiizumi.com>
2024-01-29 09:22:58 -0500

    Use React

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+const config = {
+  content: [
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/containers/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,t s,jsx,tsx,mdx}",
+  ],
+  darkMode: "class",
+  theme: {
+    extend: {},
+    plugins: [],
+  },
+};
+
+export default config;
