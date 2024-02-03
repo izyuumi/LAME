472afda82db0a606f2d9526feb8cf7df25237501
Yumi Izumi <me@yumiizumi.com>
2024-02-03 00:07:13 -0500

    remove unused vault.rs

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,16 +1,8 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-mod vault;
-
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
-
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet, vault::select_vault])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
