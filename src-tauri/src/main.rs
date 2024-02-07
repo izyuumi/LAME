07b2160d41fc584d6b9fb5fc28e2aad9fc474136
Yumi Izumi <me@yumiizumi.com>
2024-02-07 13:06:38 -0500

    feat: parse up to h6

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -6,20 +6,42 @@ fn parse_text_to_markdown(file_string: String) -> String {
     let mut result = String::new();
     let mut in_code_block = false;
     for line in file_string.lines() {
-        if let Some(first_char) = line.chars().nth(0) {
-            match first_char {
-                '#' => result.push_str(&format!("<h1>{}</h1>", &line[2..])),
-                '*' => result.push_str(&format!("<li>{}</li>", &line[2..])),
-                '`' => {
-                    if in_code_block {
-                        result.push_str("</code></pre>");
+        let chars: Vec<char> = line.chars().collect();
+        if chars.len() == 0 {
+            continue;
+        }
+        match chars[0] {
+            '#' => {
+                let mut count = 0;
+                for c in chars {
+                    if c == '#' {
+                        count += 1;
                     } else {
-                        result.push_str("<pre><code>");
+                        break;
                     }
-                    in_code_block = !in_code_block;
                 }
-                _ => result.push_str(&format!("<p>{}</p>", &line)),
+                if count > 6 {
+                    result.push_str(&format!("<p>{}</p>", &line));
+                } else {
+                    result.push_str(&format!(
+                        "<h{}>{} {}</h{}>",
+                        count,
+                        '#'.to_string().repeat(count),
+                        &line[count..],
+                        count
+                    ));
+                }
             }
+            '*' => result.push_str(&format!("<li>{}</li>", &line[2..])),
+            '`' => {
+                if in_code_block {
+                    result.push_str("</code></pre>");
+                } else {
+                    result.push_str("<pre><code>");
+                }
+                in_code_block = !in_code_block;
+            }
+            _ => result.push_str(&format!("<p>{}</p>", &line)),
         }
     }
     result
