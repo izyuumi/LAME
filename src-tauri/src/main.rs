a8cfd45cb6d00d905725cfab6cabfd7d65c090b1
Yumi Izumi <me@yumiizumi.com>
2024-02-07 13:10:49 -0500

    refactor: isolate parser

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,51 +1,9 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-#[tauri::command]
-fn parse_text_to_markdown(file_string: String) -> String {
-    let mut result = String::new();
-    let mut in_code_block = false;
-    for line in file_string.lines() {
-        let chars: Vec<char> = line.chars().collect();
-        if chars.len() == 0 {
-            continue;
-        }
-        match chars[0] {
-            '#' => {
-                let mut count = 0;
-                for c in chars {
-                    if c == '#' {
-                        count += 1;
-                    } else {
-                        break;
-                    }
-                }
-                if count > 6 {
-                    result.push_str(&format!("<p>{}</p>", &line));
-                } else {
-                    result.push_str(&format!(
-                        "<h{}>{} {}</h{}>",
-                        count,
-                        '#'.to_string().repeat(count),
-                        &line[count..],
-                        count
-                    ));
-                }
-            }
-            '*' => result.push_str(&format!("<li>{}</li>", &line[2..])),
-            '`' => {
-                if in_code_block {
-                    result.push_str("</code></pre>");
-                } else {
-                    result.push_str("<pre><code>");
-                }
-                in_code_block = !in_code_block;
-            }
-            _ => result.push_str(&format!("<p>{}</p>", &line)),
-        }
-    }
-    result
-}
+mod parser;
+
+use parser::parse_text_to_markdown;
 
 fn main() {
     tauri::Builder::default()
---
+++ src-tauri/src/parser.rs
@@ -0,0 +1,45 @@
+#[tauri::command]
+pub fn parse_text_to_markdown(file_string: String) -> String {
+    let mut result = String::new();
+    let mut in_code_block = false;
+    for line in file_string.lines() {
+        let chars: Vec<char> = line.chars().collect();
+        if chars.len() == 0 {
+            continue;
+        }
+        match chars[0] {
+            '#' => {
+                let mut count = 0;
+                for c in chars {
+                    if c == '#' {
+                        count += 1;
+                    } else {
+                        break;
+                    }
+                }
+                if count > 6 {
+                    result.push_str(&format!("<p>{}</p>", &line));
+                } else {
+                    result.push_str(&format!(
+                        "<h{}>{} {}</h{}>",
+                        count,
+                        '#'.to_string().repeat(count),
+                        &line[count..],
+                        count
+                    ));
+                }
+            }
+            '*' => result.push_str(&format!("<li>{}</li>", &line[2..])),
+            '`' => {
+                if in_code_block {
+                    result.push_str("</code></pre>");
+                } else {
+                    result.push_str("<pre><code>");
+                }
+                in_code_block = !in_code_block;
+            }
+            _ => result.push_str(&format!("<p>{}</p>", &line)),
+        }
+    }
+    result
+}
