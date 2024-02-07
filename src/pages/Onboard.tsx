4be43596436f09eef587de1270eaaa88f341e4d9
Yumi Izumi <me@yumiizumi.com>
2024-02-07 13:51:58 -0500

    fix: route to correct view when starting with a vault
    route to correct view when there was a last opened vault and that is set to the current vault path on startup

--- src/pages/Onboard.tsx
+++ src/pages/Onboard.tsx
@@ -5,59 +5,63 @@ import { appWindow } from "@tauri-apps/api/window";
 import { useNavigate } from "react-router-dom";
 
 export function Onboard() {
-	const { closeSidebar } = useSidebar();
-	const { openVaultFromPath } = useVault();
-	const navigate = useNavigate();
+  const { closeSidebar } = useSidebar();
+  const { openVaultFromPath, currentVaultPath } = useVault();
+  const navigate = useNavigate();
 
-	useEffect(() => {
-		closeSidebar();
-	}, []);
+  useEffect(() => {
+    closeSidebar();
+  }, []);
 
-	appWindow.listen("tauri://file-drop", async (event) => {
-		if (Array.isArray(event.payload) && event.payload.length > 0) {
-			await openVaultAndNavigate(event.payload[0]);
-		}
-	});
+  useEffect(() => {
+    currentVaultPath && navigate("/dashboard");
+  }, [currentVaultPath]);
 
-	const openVaultAndNavigate = async (path: string) => {
-		await openVaultFromPath(path);
-		navigate("/dashboard");
-	};
+  appWindow.listen("tauri://file-drop", async (event) => {
+    if (Array.isArray(event.payload) && event.payload.length > 0) {
+      await openVaultAndNavigate(event.payload[0]);
+    }
+  });
 
-	/**
-	 * Opens a file dialog to select a directory
-	 * @returns {Promise<void>}
-	 */
-	const selectVault = async (): Promise<void> => {
-		const selected = await open({
-			title: "Select a vault",
-			multiple: false,
-			directory: true,
-		});
-		if (selected === null) return;
-		if (Array.isArray(selected) && selected.length === 0) return;
-		if (typeof selected === "string") {
-			await openVaultAndNavigate(selected);
-		} else {
-			if (selected.length < 1) return;
-			await openVaultAndNavigate(selected[0]);
-		}
-	};
+  const openVaultAndNavigate = async (path: string) => {
+    await openVaultFromPath(path);
+    navigate("/dashboard");
+  };
 
-	return (
-		<div className="h-screen w-screen flex flex-col items-center justify-center gap-2">
-			<h1>Welcome to the LAME</h1>
-			<p>
-				<strong>L</strong>OL <strong>A</strong>nother <strong>M</strong>
-				arkdown <strong>E</strong>ditor
-			</p>
-			<p>Drag and drop a directory to get started</p>
-			<button
-				className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-				onClick={selectVault}
-			>
-				Select directory
-			</button>
-		</div>
-	);
+  /**
+   * Opens a file dialog to select a directory
+   * @returns {Promise<void>}
+   */
+  const selectVault = async (): Promise<void> => {
+    const selected = await open({
+      title: "Select a vault",
+      multiple: false,
+      directory: true,
+    });
+    if (selected === null) return;
+    if (Array.isArray(selected) && selected.length === 0) return;
+    if (typeof selected === "string") {
+      await openVaultAndNavigate(selected);
+    } else {
+      if (selected.length < 1) return;
+      await openVaultAndNavigate(selected[0]);
+    }
+  };
+
+  return (
+    <div className="h-screen w-screen flex flex-col items-center justify-center gap-2">
+      <h1>Welcome to the LAME</h1>
+      <p>
+        <strong>L</strong>OL <strong>A</strong>nother <strong>M</strong>
+        arkdown <strong>E</strong>ditor
+      </p>
+      <p>Drag and drop a directory to get started</p>
+      <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        onClick={selectVault}
+      >
+        Select directory
+      </button>
+    </div>
+  );
 }
