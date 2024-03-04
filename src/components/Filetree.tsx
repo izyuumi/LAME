f6db9f5bd80c03fe15983249e1d2f5819cfcbfda
Yumi Izumi <me@yumiizumi.com>
2024-03-04 14:45:43 -0500

    feat: add new file and folder button

--- src/components/Filetree.tsx
+++ src/components/Filetree.tsx
@@ -6,7 +6,13 @@ import {
   exists,
   createDir,
 } from "@tauri-apps/api/fs";
-import { ChevronDown, ChevronRight } from "lucide-react";
+import {
+  ChevronDown,
+  ChevronRight,
+  File,
+  FilePlus,
+  FolderPlus,
+} from "lucide-react";
 import { MouseEvent, useEffect, useRef, useState } from "react";
 import { twMerge as tm } from "tailwind-merge";
 import { watch } from "tauri-plugin-fs-watch-api";
@@ -14,38 +20,41 @@ import TitlebarSpace from "@/components/TaskbarSpace";
 import { useContextMenu } from "@/hooks";
 
 function Filetree() {
-  const { currentVaultPath } = useVault();
+  const { currentVaultPath, openPath } = useVault();
 
   const [filetree, setFiletree] = useState<FileEntry[]>([]);
   const filetreeRef = useRef<HTMLUListElement>(null);
   const [filetreeIsChanged, setFiletreeIsChanged] = useState(false);
 
+  const [showFileInput, setShowFileInput] = useState(false);
+  const [fileOrFolder, setFileOrFolder] = useState<"file" | "folder">("file");
+
+  const initMakeNewFileOrFolder = async (fileOrFolder: "file" | "folder") => {
+    setFileOrFolder(fileOrFolder);
+    setShowFileInput(true);
+  };
+
   const watchForFileChanges = async (path: string) => {
-    await watch(
-      [path],
-      async () => {
-        await getDirectoryContents(path);
-      },
-      {
-        recursive: true,
-      },
-    );
+    await watch([path], async () => await getDirectoryContents(path), {
+      recursive: true,
+    });
+  };
+
+  const sortName = (a: FileEntry, b: FileEntry) => {
+    if (a.name === b.name) return 0;
+    if (!a.name) return -1;
+    if (!b.name) return 1;
+    return a.name.localeCompare(b.name);
   };
 
   const sortRecursively = (filetree: FileEntry[]) => {
     filetree.sort((a, b) => {
       if (a.children && b.children) {
-        if (a.name === b.name) return 0;
-        if (!a.name) return -1;
-        if (!b.name) return 1;
-        return a.name.localeCompare(b.name);
+        return sortName(a, b);
       }
       if (a.children && !b.children) return -1;
       if (!a.children && b.children) return 1;
-      if (a.name === b.name) return 0;
-      if (!a.name) return -1;
-      if (!b.name) return 1;
-      return a.name.localeCompare(b.name);
+      return sortName(a, b);
     });
     filetree.forEach((file) => {
       if (file.children) {
@@ -75,15 +84,26 @@ function Filetree() {
     }
   }, [filetreeIsChanged]);
 
+  if (!currentVaultPath) return null;
+
   return (
     <div className="bg-base-300 h-screen">
       <TitlebarSpace />
+      <div className="w-full flex justify-center gap-2">
+        <div className="tooltip tooltip-bottom" data-tip="New File">
+          <button onClick={() => initMakeNewFileOrFolder("file")}>
+            <FilePlus size={20} />
+          </button>
+        </div>
+        <div className="tooltip tooltip-bottom" data-tip="New Folder">
+          <button onClick={() => initMakeNewFileOrFolder("folder")}>
+            <FolderPlus size={20} />
+          </button>
+        </div>
+      </div>
       <ul
         ref={filetreeRef}
-        className={tm(
-          "bg-base-300 flex w-[150px] select-none flex-col p-1",
-          !currentVaultPath && "hidden",
-        )}
+        className={tm("bg-base-300 flex w-[150px] select-none flex-col p-1")}
       >
         {filetree.map(
           (file) =>
@@ -94,7 +114,22 @@ function Filetree() {
                 {...file}
                 updateFiletree={() => setFiletreeIsChanged(true)}
               />
-            ),
+            )
+        )}
+        {showFileInput && (
+          <FiletreeInput
+            updateFiletree={() => setFiletreeIsChanged(true)}
+            onCanceled={() => setShowFileInput(false)}
+            onChanged={(newPath) =>
+              makeNewFileOrFolder(
+                newPath,
+                currentVaultPath,
+                fileOrFolder,
+                () => getDirectoryContents(currentVaultPath),
+                openPath
+              )
+            }
+          />
         )}
       </ul>
     </div>
@@ -120,85 +155,41 @@ const FiletreeItem = ({
 
   const { openContextMenu, closeContextMenu } = useContextMenu();
 
-  const showNewFileInputLiRef = useRef<HTMLLIElement>(null);
   const [showNewFileInput, setShowNewFileInput] = useState(false);
-  const [newFileName, setNewFileName] = useState("");
-  const makeNewFileOrFolder = async (fileOrFolder: "file" | "folder") => {
+  const [fileOrFolder, setFileOrFolder] = useState<"file" | "folder">("file");
+
+  const initMakeNewFileOrFolder = async (fileOrFolder: "file" | "folder") => {
     closeContextMenu();
+    setFileOrFolder(fileOrFolder);
     setShowNewFileInput(true);
-    setIsOpen(true);
-    setTimeout(() => {
-      const input = showNewFileInputLiRef.current?.querySelector("input");
-      if (!input) return;
-      input.focus();
-      input.addEventListener("blur", async () => {
-        setNewFileName("");
-        setShowNewFileInput(false);
-      });
-      input.addEventListener("keydown", async (e) => {
-        if (e.key === "Enter") {
-          e.preventDefault();
-          const newFileName = input.value;
-          const newFilePath = `${path}/${newFileName}`;
-          const fileAlreadyExists = await exists(newFilePath);
-          if (!fileAlreadyExists) {
-            switch (fileOrFolder) {
-              case "file":
-                await writeTextFile(newFilePath, "");
-                break;
-              case "folder":
-                if (newFileName.endsWith("/")) {
-                  await createDir(newFilePath);
-                  break;
-                }
-                await createDir(`${newFilePath}/`);
-                break;
-            }
-            updateFiletree();
-          }
-          if (fileOrFolder === "file") {
-            openPath(newFilePath);
-          }
-          setNewFileName("");
-          setShowNewFileInput(false);
-        }
-      });
-    }, 50);
   };
 
-  const FiletreeDirectoryContextMenu = () => {
-    return (
-      <>
-        <ContextMenuItem onClick={() => makeNewFileOrFolder("file")}>
-          New File
-        </ContextMenuItem>
-        <ContextMenuItem onClick={() => makeNewFileOrFolder("folder")}>
-          New Folder
-        </ContextMenuItem>
-      </>
-    );
-  };
+  const FiletreeDirectoryContextMenu = () => (
+    <>
+      <ContextMenuItem onClick={() => initMakeNewFileOrFolder("file")}>
+        New File
+      </ContextMenuItem>
+      <ContextMenuItem onClick={() => initMakeNewFileOrFolder("folder")}>
+        New Folder
+      </ContextMenuItem>
+    </>
+  );
 
   const directoryContextMenu = (event: MouseEvent<HTMLButtonElement>) => {
     event.preventDefault();
     openContextMenu(event, itemRef.current, <FiletreeDirectoryContextMenu />);
   };
 
-  const FiletreeItemContextMenu = () => {
-    return (
-      <>
-        <ContextMenuItem onClick={() => console.log("Open")}>
-          Open
-        </ContextMenuItem>
-        <ContextMenuItem onClick={() => console.log("Rename")}>
-          Rename
-        </ContextMenuItem>
-        <ContextMenuItem onClick={() => console.log("Delete")}>
-          Delete
-        </ContextMenuItem>
-      </>
-    );
-  };
+  const FiletreeItemContextMenu = () => (
+    <>
+      <ContextMenuItem onClick={() => console.log("Rename")}>
+        Rename
+      </ContextMenuItem>
+      <ContextMenuItem onClick={() => console.log("Delete")}>
+        Delete
+      </ContextMenuItem>
+    </>
+  );
 
   const fileContextMenu = (event: MouseEvent<HTMLButtonElement>) => {
     event.preventDefault();
@@ -206,50 +197,120 @@ const FiletreeItem = ({
   };
 
   return (
-    <>
-      <li className="ml-2" ref={itemRef}>
-        <button
-          onClick={() =>
-            isDirectory ? setIsOpen((prev) => !prev) : openPath(path)
-          }
-          onContextMenu={isDirectory ? directoryContextMenu : fileContextMenu}
-          className={tm(
-            "flex w-full items-center",
-            openedPath === path && "text-[#0052ff]",
-            !isDirectory && "ml-3",
-          )}
-        >
-          {isDirectory &&
-            (isOpen ? (
-              <ChevronDown size={16} className="mr-1 opacity-40" />
-            ) : (
-              <ChevronRight size={16} className="mr-1 opacity-40" />
-            ))}
-          {name}
-        </button>
-        {isDirectory && (
-          <ul>
-            {showNewFileInput && (
-              <li ref={showNewFileInputLiRef} className="ml-3">
-                <input
-                  className="w-full"
-                  type="text"
-                  value={newFileName}
-                  onChange={(e) => setNewFileName(e.target.value)}
-                />
-              </li>
-            )}
-            {children.map(
-              (child) =>
-                !child.name?.startsWith(".") && (
-                  <span key={child.path} className={isOpen ? "" : "hidden"}>
-                    <FiletreeItem {...child} updateFiletree={updateFiletree} />
-                  </span>
-                ),
-            )}
-          </ul>
+    <li className="ml-2" ref={itemRef}>
+      <button
+        onClick={() =>
+          isDirectory ? setIsOpen((prev) => !prev) : openPath(path)
+        }
+        onContextMenu={isDirectory ? directoryContextMenu : fileContextMenu}
+        className={tm(
+          "flex w-full items-center",
+          openedPath === path && "text-[#0052ff]",
+          !isDirectory && "ml-3"
         )}
-      </li>
-    </>
+      >
+        {isDirectory &&
+          (isOpen ? (
+            <ChevronDown size={16} className="mr-1 opacity-40" />
+          ) : (
+            <ChevronRight size={16} className="mr-1 opacity-40" />
+          ))}
+        {name}
+      </button>
+      {isDirectory && (
+        <ul>
+          {showNewFileInput && (
+            <FiletreeInput
+              updateFiletree={updateFiletree}
+              onCanceled={() => setShowNewFileInput(false)}
+              onChanged={(newPath) =>
+                makeNewFileOrFolder(
+                  newPath,
+                  path,
+                  fileOrFolder,
+                  updateFiletree,
+                  openPath
+                )
+              }
+            />
+          )}
+          {children.map(
+            (child) =>
+              !child.name?.startsWith(".") && (
+                <span key={child.path} className={isOpen ? "" : "hidden"}>
+                  <FiletreeItem {...child} updateFiletree={updateFiletree} />
+                </span>
+              )
+          )}
+        </ul>
+      )}
+    </li>
+  );
+};
+
+const makeNewFileOrFolder = async (
+  newName: string,
+  path: string,
+  fileOrFolder: "file" | "folder",
+  updateFiletree: () => void,
+  openPath: (path: string) => void
+) => {
+  const newPath = `${path}/${newName}`;
+  const fileAlreadyExists = await exists(newPath);
+  if (!fileAlreadyExists) {
+    switch (fileOrFolder) {
+      case "file":
+        await writeTextFile(newPath, "");
+        break;
+      case "folder":
+        if (newPath.endsWith("/")) {
+          await createDir(newPath);
+          break;
+        }
+        await createDir(`${newPath}/`);
+        break;
+    }
+    updateFiletree();
+  }
+  if (fileOrFolder === "file") {
+    openPath(newPath);
+  }
+};
+
+interface FiletreeInputProps {
+  path?: string;
+  updateFiletree: () => void;
+  onCanceled: () => void;
+  onChanged: (newPath: string) => void;
+}
+
+const FiletreeInput = (props: FiletreeInputProps) => {
+  const { path, updateFiletree, onCanceled, onChanged } = props;
+
+  const [value, setValue] = useState(path ?? "");
+
+  const handleBlur = () => {
+    if (value === "") return onCanceled();
+    onChanged(value);
+    updateFiletree();
+    onCanceled();
+  };
+
+  const handleKeyDown = (ev: React.KeyboardEvent<HTMLInputElement>) => {
+    if (ev.key === "Enter") handleBlur();
+  };
+
+  return (
+    <li className="ml-3">
+      <input
+        className="w-full"
+        type="text"
+        value={value}
+        autoFocus
+        onChange={(ev) => setValue(ev.target.value)}
+        onBlur={handleBlur}
+        onKeyDown={handleKeyDown}
+      />
+    </li>
   );
 };
