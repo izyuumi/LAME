30f2f03453cc3c67b25aaf30e121a7f1f6ee6258
yutatokoi <yutatokoi@proton.me>
2024-03-06 09:47:35 +1100

    feat: color themes from daisyUI

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,7 @@
 const config = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   // darkMode: "class",
-  theme: {
-    extend: {},
-  },
+  themes: ["wireframe", "black",],
   plugins: [require("daisyui")],
 };
 
