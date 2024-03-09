730c7fffed3c91b890aa21cef85b50b195a34cfd
Yumi Izumi <me@yumiizumi.com>
2024-03-08 23:56:52 -0500

    feat(parser): emit errors when parser return errors

--- src-tauri/src/parser.rs
+++ src-tauri/src/parser.rs
@@ -1,4 +1,5 @@
-use html2md_rs::{parser::parse_html, to_md::to_md};
+use html2md_rs::to_md::safe_from_html_to_md;
+use tauri::api::dialog::message;
 
 #[tauri::command]
 pub fn parse_markdown_as_html(file_string: String) -> String {
@@ -6,6 +7,16 @@ pub fn parse_markdown_as_html(file_string: String) -> String {
 }
 
 #[tauri::command]
-pub fn parse_html_as_markdown(html_string: String) -> String {
-    to_md(parse_html(html_string))
+pub fn parse_html_as_markdown(html_string: String, window: tauri::Window) -> String {
+    match safe_from_html_to_md(html_string) {
+        Ok(md) => md,
+        Err(e) => {
+            message(
+                Some(&window),
+                "Error parsing HTML to Markdown",
+                format!("Error: {:?}.", e),
+            );
+            return "".to_string();
+        }
+    }
 }
