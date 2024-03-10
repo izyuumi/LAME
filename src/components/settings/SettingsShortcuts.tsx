d5ddb8d2aa19458e329dfc19b7bfb1a73a77c0d8
Yumi Izumi <me@yumiizumi.com>
2024-03-10 00:50:59 -0500

    feat: add shortcut recording indicator

--- src/components/settings/SettingsShortcuts.tsx
+++ src/components/settings/SettingsShortcuts.tsx
@@ -1,20 +1,19 @@
 import { type Command, useCmdk } from "@/hooks";
 import { useRecordHotkeys } from "react-hotkeys-hook";
-import Kbd from "../common/Kbd";
+import Kbd from "@/components/common/Kbd";
 import { useEffect } from "react";
+import { Disc } from "lucide-react";
 
 function SettingsShortcuts() {
   const { cmdkCommands } = useCmdk();
 
   return (
-    <>
-      <ul>
-        <h2 className="text-lg py-2 font-semibold">Shortcuts</h2>
-        {Object.entries(cmdkCommands).map(([id, command]) => (
-          <SettingsShortcutsItem key={id} id={id} command={command} />
-        ))}
-      </ul>
-    </>
+    <ul>
+      <h2 className="text-lg py-2 font-semibold">Shortcuts</h2>
+      {Object.entries(cmdkCommands).map(([id, command]) => (
+        <SettingsShortcutsItem key={id} id={id} command={command} />
+      ))}
+    </ul>
   );
 }
 
@@ -27,10 +26,15 @@ const SettingsShortcutsItem = ({
   id: string;
   command: Command;
 }) => {
-  const [keys, { start, stop, isRecording }] = useRecordHotkeys();
+  const [keys, { start, stop, isRecording, resetKeys }] = useRecordHotkeys();
   const { addCmdkCommand } = useCmdk();
 
   useEffect(() => {
+    if (keys.has("escape")) {
+      resetKeys();
+      stop();
+      return;
+    }
     if (
       keys.size > 0 &&
       !Array.from(keys).every((key) => modifiers.includes(key))
@@ -42,7 +46,16 @@ const SettingsShortcutsItem = ({
 
   return (
     <li className="flex justify-between h-12 items-center">
-      <p>{command.label}</p>
+      <div className="flex gap-4 items-center">
+        <p>{command.label}</p>
+        {isRecording && (
+          <div className="tooltip" data-tip="Click to stop recording">
+            <button onClick={stop}>
+              <Disc size={16} color="red" />
+            </button>
+          </div>
+        )}
+      </div>
       <button
         onClick={() => {
           if (isRecording) {
@@ -53,7 +66,9 @@ const SettingsShortcutsItem = ({
         }}
       >
         {isRecording
-          ? Array.from(keys).map((key) => <Kbd key={key} k={key} />)
+          ? Array.from(keys).map(
+              (key) => key !== "escape" && <Kbd key={key} k={key} />
+            )
           : command.key.split("+").map((key) => <Kbd key={key} k={key} />)}
       </button>
     </li>
