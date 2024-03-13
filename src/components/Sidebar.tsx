0f47c8c1dda9df15f4e21c96591461b068b33a10
Yumi Izumi <me@yumiizumi.com>
2024-03-13 16:17:48 -0400

    fix(sidebar): make sidebar buttons smaller

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -6,63 +6,59 @@ import VaultPrompt from "./VaultPrompt";
 import { shortcutKeysToString } from "./common/Kbd";
 
 function Sidebar() {
-  const { openSettings } = useSettings();
-  const { setInterfaceContext, findCmdkCommand } = useCmdk();
+	const { openSettings } = useSettings();
+	const { setInterfaceContext, findCmdkCommand } = useCmdk();
 
-  const vaultPromptRef = useRef<HTMLDialogElement>(null);
+	const vaultPromptRef = useRef<HTMLDialogElement>(null);
 
-  return (
-    <>
-      <div className="bg-base-200 flex flex-col">
-        <TitlebarSpace />
-        <div className="flex-1" />
-        <ul className="menu text-base-content p-1">
-          <SidebarItem
-            icon={<Vault size={16} />}
-            text="Vault"
-            onClick={() => {
-              vaultPromptRef.current?.showModal();
-              setInterfaceContext("vault-prompt");
-              const firstFocusable = vaultPromptRef.current?.querySelector(
-                "button, [href], input, select, textarea, [tabindex]:not([tabindex='-1'])"
-              ) as HTMLElement;
-              firstFocusable?.blur();
-            }}
-          />
-          <SidebarItem
-            icon={<Settings size={16} />}
-            text={`Settings ${shortcutKeysToString(
-              findCmdkCommand("openSettings")?.key ?? ""
-            )}`}
-            onClick={openSettings}
-          />
-        </ul>
-      </div>
-      <VaultPrompt dialogRef={vaultPromptRef} />
-    </>
-  );
+	return (
+		<>
+			<div className="bg-base-200 flex flex-col">
+				<TitlebarSpace />
+				<div className="flex-1" />
+				<ul className="menu text-base-content p-1 pb-4">
+					<SidebarItem
+						icon={<Vault size={16} />}
+						text="Vault"
+						onClick={() => {
+							vaultPromptRef.current?.showModal();
+							setInterfaceContext("vault-prompt");
+							const firstFocusable = vaultPromptRef.current?.querySelector(
+								"button, [href], input, select, textarea, [tabindex]:not([tabindex='-1'])",
+							) as HTMLElement;
+							firstFocusable?.blur();
+						}}
+					/>
+					<SidebarItem
+						icon={<Settings size={16} />}
+						text={`Settings ${shortcutKeysToString(
+							findCmdkCommand("openSettings")?.key ?? "",
+						)}`}
+						onClick={openSettings}
+					/>
+				</ul>
+			</div>
+			<VaultPrompt dialogRef={vaultPromptRef} />
+		</>
+	);
 }
 
 function SidebarItem({
-  icon,
-  text,
-  onClick,
+	icon,
+	text,
+	onClick,
 }: Readonly<{
-  icon: React.ReactNode;
-  text: string;
-  onClick?: () => void;
+	icon: React.ReactNode;
+	text: string;
+	onClick?: () => void;
 }>) {
-  return (
-    <li>
-      <button
-        onClick={onClick}
-        className="tooltip tooltip-right"
-        data-tip={text}
-      >
-        <span className="p-2">{icon}</span>
-      </button>
-    </li>
-  );
+	return (
+		<li className="tooltip tooltip-right" data-tip={text}>
+			<button onClick={onClick} className="p-2">
+				<span>{icon}</span>
+			</button>
+		</li>
+	);
 }
 
 export default Sidebar;
